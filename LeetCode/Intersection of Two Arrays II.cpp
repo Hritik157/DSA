448d7d3e474d8d851c00fd1ce15b9b084365ea6f
Hritik Sharma <57637086+Hritik157@users.noreply.github.com>
2021-09-17 22:34:05 +0530

    Create Intersection of Two Arrays II.cpp

---
+++ LeetCode/Intersection of Two Arrays II.cpp
@@ -0,0 +1,22 @@
+/*
+   https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+*/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> freq;
+        vector<int> res;
+        
+        for (auto num : nums1) freq[num]++;
+        
+        for (auto num : nums2) {
+            if (freq[num]) {
+                res.push_back(num);
+                freq[num]--;
+            }
+        }
+        
+        return res;
+    }
+};
